d1f82bb78306f53bdfcfd96348b4b8c3dc92ab7b
alejandrogsk <alejandrogsk9900@gmail.com>
2022-08-03 09:35:29 -0300

    primer commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  i18n: {
+    locales: ['es', 'en'],
+    defaultLocale: 'es',
+    localeDetection: true,
+  },
+  
 }
 
 module.exports = nextConfig
